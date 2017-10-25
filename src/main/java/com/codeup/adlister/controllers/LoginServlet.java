7216c031a31ea168186da7056d911d6ef7ca77ea
michael <mgebregziabher86@gmail.com>
2017-10-25 15:10:48 -0500

    Created loginServlet and login file inside WEB_INF and tested

---
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -0,0 +1,47 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.util.Password;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet(name = "controllers.LoginServlet", urlPatterns = "/login")
+@WebServlet(name = "ontrollers.LoginServlet", urlPatterns = "/login")
+ public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        if (request.getSession().getAttribute("user") != null) {
+//            response.sendRedirect("/profile");
+//            return;
+//        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+    }
+}
+
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+
+
+
+//                User user = DaoFactory.getUsersDao().findByUsername(username);
+
+//                if (user == null) {
+//                    response.sendRedirect("/login");
+//                    return;
+//                }
+//
+//                boolean passwordsMatch = Password.check(password, user.getPassword());
+//
+//                if (passwordsMatch) {
+//                    request.getSession().setAttribute("user", user);
+//                    response.sendRedirect("/profile");
+//                } else {
+//                    response.sendRedirect("/login");
+//                }
+//            }
+//        }
+
