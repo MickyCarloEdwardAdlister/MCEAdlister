26db7b1e619d9cf1c623c474e5159422e71fcda0
Edward Delgado Jr <edward.delgado@icloud.com>
2017-10-26 10:01:38 -0500

    ShowAdsServlet created

---
+++ src/main/java/com/codeup/adlister/controllers/ShowAdsServlet.java
@@ -0,0 +1,29 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "servlets.ShowAdsServlet", urlPatterns = "/ad")
+public class ShowAdsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+
+        List<Ad> ads = DaoFactory.getAdsDao().all();
+
+        session.setAttribute("firstAd", ads.get(0).getTitle());
+
+        request.setAttribute("ads", ads);
+
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+    }
+}
