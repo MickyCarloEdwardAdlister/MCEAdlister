bdadcf50cc1bce8d2c8e1caaa0b742ecc8102637
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-26 09:37:59 -0500

    register servlet finished uses request.setattribute to error with if statement in html declaring that all values must be filles if input is empty

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,17 +1,40 @@
 package com.codeup.adlister.controllers;
+import java.awt.*;
+
+import javafx.scene.control.Alert;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
 import java.io.IOException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
-public class RegisterServlet extends HttpServlet{
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        request.getRequestDispatcher("WEB-INF/register.jsp").forward(request,response);
+public class RegisterServlet extends HttpServlet {
+    private Component frame;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String confirm_password = request.getParameter("confirm_password");
 
+
+        boolean inputErrors =
+                        username.isEmpty()
+                        || email.isEmpty()
+                        || password.isEmpty()
+                        || confirm_password.isEmpty();
+
+        if (inputErrors) {
+            request.setAttribute("error", "All the values are required");
+            request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request,response);
+        }
+    }
 }
--- src/main/webapp/WEB-INF/register.jsp
+++ src/main/webapp/WEB-INF/register.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: carlocappellini
@@ -12,10 +13,14 @@
 <title>register new user</title>
 </head>
 <body>
-<form>
+
     <jsp:include page="partials/head.jsp" />
     <div class="container">
         <h1>Please Register</h1>
+
+        <c:if test="${error != null}">
+            <p>${error}</p>
+        </c:if>
 
         <form action="/register" method="post">
             <div class="form-group">
@@ -27,14 +32,19 @@
             </div>
 
             <div class="form-group">
-                password : <input id="password" class="form-control" type="password">
+                password : <input id="password" class="form-control" name="password" type="password">
+            </div>
+
+            <div class="form-group">
+                <label for="confirm_password">Confirm Password</label>
+                <input id="confirm_password" name="confirm_password" class="form-control" type="password">
             </div>
             <button type="submit">Register</button>
 
         </form>
 
     </div>
-</form>
+
 
 </body>
 </html>
