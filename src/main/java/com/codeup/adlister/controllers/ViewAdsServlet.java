d807c86e12cb7fe1484072fe9da447293e898654
Edward Delgado Jr <edward.delgado@icloud.com>
2017-10-26 09:52:44 -0500

    ViewAdsServlet created

--- src/main/java/com/codeup/adlister/controllers/ViewAdsServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewAdsServlet.java
@@ -1,4 +1,20 @@
 package com.codeup.adlister.controllers;
 
-public class ViewAdsServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
+public class ViewAdsServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // If the user is not logged in
+        if (request.getSession().getAttribute("user") == null ) {
+            response.sendRedirect("/login");
+        } else {
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        }
+    }
 }
