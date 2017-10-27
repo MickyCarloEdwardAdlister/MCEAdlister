9f398206a4fcdeb7a37ccf091524acb57922cbe6
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-27 12:12:05 -0500

    sql users dao

--- src/main/java/com/codeup/adlister/controllers/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/controllers/MySQLUsersDao.java
@@ -1,4 +1,8 @@
+package com.codeup.adlister.controllers;
+
+import java.sql.Connection;package com.codeup.adlister.dao;
 import Daos.Config;
+import com.codeup.adlister.dao.Users;
 import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
 
@@ -19,4 +23,58 @@ public class MySQLUsersDao implements Users {
             throw new RuntimeException("Error connecting to the database!", e);
         }
     }
-}
+
+    @Override
+    public User findByUsername(String username)  {
+        String SearchQuery = "Select * from Users where username = ?";
+
+        try {
+
+
+            PreparedStatement statement = connection.prepareStatement(SearchQuery);
+            statement.setString(1, username);
+            return null;
+        }catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error finding users", e);
+        }
+
+    }
+
+    @Override
+    public Long insert(User user) {
+        String usersCreateQuery = "insert into users(username, email, password) values(?,?,?)";
+
+        try{
+            PreparedStatement statement = connection.prepareStatement(usersCreateQuery,Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1,user.getUsername);
+            statement.setString(3,user.getEmail);
+            statement.setString(2, user.getPassword);
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+
+
+            return rs.getLong(1);
+
+
+        }catch (SQLException e){
+           throw new RuntimeException("Error creating user", e);
+
+
+        }
+    }
+    private Users extractUser(ResultSet rs) throws SQLException {
+        if (!rs.next()) {
+            return null
+
+
+        }
+        return new User(
+                rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password")
+
+        );
+        }
+    }
--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -1,25 +1,27 @@
 package com.codeup.adlister.dao;
 
 import Daos.Config;
+import com.codeup.adlister.controllers.MySQLUsersDao;
 
 public class DaoFactory {
     private static Ads adsDao;
-//    private static Users usersDao;
+    private static Users usersDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
-//            adsDao = new MySQLAdsDao(config);
+            adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
     }
 
     // Factory method for Users
-//    public static Users getUsersDao() {
-        // We're making sure we only have one Users dao
-//        if (usersDao == null) {
-//            usersDao = new MySQLUsersDao(config);
-//        }
-//        return usersDao;
+    public static Users getUsersDao() {
+// We're making sure we only have one Users dao
+        if (usersDao == null) {
+            usersDao = new MySQLUsersDao(config);
+        }
+            return usersDao;
+        }
     }
-//}
+
