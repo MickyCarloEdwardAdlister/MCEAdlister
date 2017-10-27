4776953b3d70c437168243b573d46279312aa8c0
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-27 11:08:23 -0500

    mysqlusersdao

---
+++ src/main/java/com/codeup/adlister/controllers/MySQLUsersDao.java
@@ -0,0 +1,22 @@
+import Daos.Config;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users {
+    private Connection connection;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+}
