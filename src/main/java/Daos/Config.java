27284a21dd6fd761823dc6b14fe3b7f4181566c1
michael <mgebregziabher86@gmail.com>
2017-10-27 13:01:19 -0500

    Created Models and MySQLAdsDao

--- src/main/java/Daos/Config.java
+++ src/main/java/Daos/Config.java
@@ -4,4 +4,16 @@ public class Config {
     private static String url = "jdbc:mysql://localhost/mce_db?serverTimezone=UTC";
     private static String username = "mce";
     private static String password = "mce";
+
+    public static String getUrl() {
+        return url;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
 }
--- src/main/java/com/codeup/adlister/models/Ad.java
+++ src/main/java/com/codeup/adlister/models/Ad.java
@@ -1,4 +1,55 @@
 package com.codeup.adlister.models;
 
 public class Ad {
+    private Long id;
+    private Long userId;
+    private String title;
+    private String description;
+
+    public Ad() { }
+
+    public Ad(Long userId, String title, String description) {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Ad(Long id, Long userId, String title, String description) {
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
