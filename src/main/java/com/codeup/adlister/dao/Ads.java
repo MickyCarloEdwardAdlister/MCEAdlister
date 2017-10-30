d2f4d9681077b2b9853ec902b4e4ae978e6938f5
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-30 11:34:02 -0500

    user can create ads in their profile and view their created ads - [x ] Login/Logout - [x] Register a new user - [x] Post a new ad - [x] Show all the ads - [ ] View single ad details - [ ] Search ads - [ ] View your ads in your profile page - [ ] Update ads information - [ ] Delete ads - [ ] Update profile information (change email, name) - [ ] Change password - [ ] Categorize your ad - [ ] Show only either the login or logout link your nav bar - [ ] Validate the input and show error messages in your forms - [ ] Sticky forms, if validation fails, the values previously entered should stick in the form - [ ] Improve your visual design with bootstrap or another framework - [ ] Ensure usernames are unique (database and application level, ie. Java validation) - [ ] Create base class for DAOs

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -2,11 +2,13 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.Ad;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface Ads {
     List<Ad> all();
     Long insert(Ad ad);
     List<Ad> search(String searchAd);
+    List<Ad> findByUsername(long user_id);
 }
 
